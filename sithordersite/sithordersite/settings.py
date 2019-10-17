1bbc93d71af585dbc76fab758c75cbd6c2e5818a
Marat Gizzatullin <maratgiz@gmail.com>
2019-10-17 14:40:42 +0300

    changed SECRET_KEY and DEBUG vars in settings.py

--- sithordersite/sithordersite/settings.py
+++ sithordersite/sithordersite/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'te3=5r2!+2_@qs)x92cq(bi@g*=q9@vi=l73-nj_w9h&b&0ff('
+# SECRET_KEY = 'te3=5r2!+2_@qs)x92cq(bi@g*=q9@vi=l73-nj_w9h&b&0ff('
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'te3=5r2!+2_@qs)x92cq(bi@g*=q9@vi=l73-nj_w9h&b&0ff(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = []
 
